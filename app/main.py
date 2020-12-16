d0425ecc0c7ce6c067b11650177921dfcecbf583
mugenga <rockyyves@gmail.com>
2020-12-16 23:17:16 +0200

    final changes

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from app import schemas, crud
 
 app = FastAPI(debug=True)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Main endpoint for loan prediction
 @app.post("/model")
@@ -11,6 +19,12 @@ def get_loan(features: schemas.Features):
     # Method call upon the prediction model
     loan = crud.get_loan(features)
     return loan
+
+
+# Main endpoint for loan prediction
+@app.get("/")
+def get_loan():
+    return {"Hello": "Welcome to our API visit .../docs for the documentation"}
 
 
 
